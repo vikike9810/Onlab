b107e581b4fd3cb455e1e62adbf2cbc4dd1a6ceb
Kulcsi <tommahoch@gmail.com>
2019-02-25 20:33:16 +0100

    settings

---
+++ app/src/main/java/com/onlab/gymapp/Profile/User.kt
@@ -0,0 +1,13 @@
+package com.onlab.gymapp.Profile
+
+import com.google.android.gms.flags.Flag
+import java.util.*
+
+object User {
+
+    lateinit var Name : String
+    lateinit var Height : Integer
+    lateinit var Weight : Integer
+    lateinit var Birth : Date
+
+}
