cb95ea16ce5b1c5b326ace5b573d74ea0071614e
vikike9810 <vikike9810@gmail.com>
2019-04-26 14:10:01 +0200

    room

--- app/src/main/java/com/onlab/gymapp/Training/TodayActivity.kt
+++ app/src/main/java/com/onlab/gymapp/Training/TodayActivity.kt
@@ -4,45 +4,74 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
+
 import com.onlab.gymapp.R
-import kotlinx.android.synthetic.main.activity_today.*
 import kotlinx.android.synthetic.main.trainings_recycler_view.*
+import com.onlab.gymapp.Dao.TrainingDatabase
+
 
 class TodayActivity : AppCompatActivity(),AddFragment.TrainingCreatedListener {
 
 
-    val trainings: ArrayList<Training> = ArrayList()
+    var trainings: ArrayList<Training> = ArrayList()
     var adapter=TrainingAdapter(trainings, this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_today)
-        loadTrainings()
+        loadItems2()
+
 
         // Creates a vertical Layout Manager
-        rv_training_list.layoutManager = LinearLayoutManager(this)
 
         // Access the RecyclerView Adapter and load the data into it
-        rv_training_list.adapter = adapter
+
 
     }
 
-    private fun loadTrainings() {
-        trainings.add(Training(Training_Type.Kardio,20,200))
-        trainings.add(Training(Training_Type.Sulyzos_edzes,30,300))
-        trainings.add(Training(Training_Type.Nyujtas,10,100))
-        trainings.add(Training(Training_Type.Kardio,30,300))
-        trainings.add(Training(Training_Type.Nyujtas,30,300))
-        trainings.add(Training(Training_Type.Sulyzos_edzes,30,300))
+    private fun loadItems() {
+        adapter.AddTraining(Training(null,Training_Type.Kardio.toString(),20,200))
+        adapter.AddTraining(Training(null,Training_Type.Sulyzos_edzes.toString(),30,300))
+        adapter.AddTraining(Training(null,Training_Type.Nyujtas.toString(),10,100))
+        adapter.AddTraining(Training(null,Training_Type.Kardio.toString(),30,300))
+        adapter.AddTraining(Training(null,Training_Type.Nyujtas.toString(),30,300))
+        adapter.AddTraining(Training(null,Training_Type.Sulyzos_edzes.toString(),30,300))
+        rv_training_list.adapter = adapter
+        rv_training_list.layoutManager = LinearLayoutManager(this)
     }
 
     override fun onTrainingCreated(training: Training) {
-        adapter.AddTraining(training)
+
+        val dbThread = Thread {
+            val id = TrainingDatabase.getAppDataBase(this)!!.TrainingDao().insert(training)
+            training.itemId = id
+            runOnUiThread{
+                adapter.AddTraining(training)
+            }
+        }
+        dbThread.start()
+       // adapter.AddTraining(training)
     }
 
     fun FloatingAdd(v: View){
         val todoCreateFragment = AddFragment()
         todoCreateFragment.show(supportFragmentManager, "TAG")
+    }
+
+ private fun loadItems2() {
+
+        val dbThread = Thread {
+            val items = TrainingDatabase.getAppDataBase(this)!!.TrainingDao().getAll() ?:null
+            runOnUiThread{
+                //adapter.itemClickListener = this
+                if(!(items==null)) {
+                   adapter.addAll(items)
+                }
+                rv_training_list.adapter = adapter
+                rv_training_list.layoutManager = LinearLayoutManager(this)
+            }
+        }
+        dbThread.start()
     }
 
 
--- app/src/main/java/com/onlab/gymapp/Training/Training_Type.kt
+++ app/src/main/java/com/onlab/gymapp/Training/Training_Type.kt
@@ -1,5 +1,24 @@
 package com.onlab.gymapp.Training
 
-enum class Training_Type {
-    Kardio, Sulyzos_edzes, Nyujtas
-}
+
+
+/*enum class Training_Type {
+    Kardio, Sulyzos_edzes, Nyujtas;
+
+    @TypeConverter
+    fun getByOrdinal(ordinal: Int): Training_Type? {
+        var ret: Training_Type? = null
+        for (cat in Training_Type.values()) {
+            if (cat.ordinal == ordinal) {
+                ret = cat
+                break
+            }
+        }
+        return ret
+    }
+
+    @TypeConverter
+    fun toInt(category: Training_Type): Int {
+        return category.ordinal
+    }
+}*/
